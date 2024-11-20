3e22b0097d21df630e4d7b0355aaac0b3cff6748
Fishuan <acs111111@gm.ntcu.edu.tw>
2024-11-21 00:05:39 +0800

    fix改良版~20x20可以

--- Nonograms.java
+++ Nonograms.java
@@ -6,16 +6,14 @@ import java.util.List;
 import java.util.Scanner;
 
 public class Nonograms {
-    static int X = 10;
-    // static int[][] G0, G1, G2 = new int[X][X]; // 圖
-
+    static int X = 20;
+    static int[][] G0, G1, G = new int[X][X]; // 圖
+    static String[][] ans = new String[2][X];
     static int statusG = 0; // -1 = CONFLICT; 0 = INCOMPLETE; 1 = COMPLETE.
 
     public static void main(String[] args) {
         try {
-            int[][] G = new int[X][X];
-            String[][] ans = new String[2][X];
-            File file = new File("Nonograms.txt");
+            File file = new File("Nonogram.txt");
             Scanner scanner = new Scanner(file);
 
             for (int i = 0; i < X; i++) {
@@ -41,9 +39,9 @@ public class Nonograms {
 
             // 測試fix&paint
             // LinkedList<Integer> D = new LinkedList<>();
-            // int[] L = { -1, 1, 0, 1, -1 };
-            // D.add(1);
-            // D.add(2);
+            // int[] L = { 1, 1, -1, -1, 1, -1, -1, -1, -1, -1 };
+            // D.add(6);
+            // D.add(3);
             // for (int p : Paint(L.length, D.size(), D, L)) {
             // System.out.printf("%3d", p);
             // }
@@ -52,28 +50,112 @@ public class Nonograms {
         }
     }
 
-    // // 這我還沒動
-    // static void FP(int[][] G) {
-    // G0 = G.clone();
-    // G1 = G.clone();
-    // // status 0 = 無解; 1=有解; 2=沒解完...不知道啦我先這樣定義
-    // // PROPAGATE(G);
-    // if (status(G) == 0 || status(G) == 1) {
-    // return;
+    // FP
+    // static int[][] FP1(String[][] ans) {
+    // int newp = 0;
+    // do {
+    // int[][] IIG = new int[X][X];
+    // IIG = PROPAGATE(ans, IIG, G, statusG);
+    // if (statusG == -1 || statusG == 1) {
+    // return G;
     // }
-    // UPDATEONALLG(G);
-    // // for (所有p) {
-    // // PROBE(p);
-    // // if (status(G) == 0 || status(G) == 1) {
-    // // return;
+    // newp = UPDATEONALLG(IIG);
+    // System.out.println("PPG:\n-------------------");
+    // for (int i = 0; i < X; i++) {
+    // for (int j = 0; j < X; j++) {
+    // System.out.printf("%3d", G[i][j]);
+    // }
+    // System.out.println();
+    // }
+    // while (!P.isEmpty()) {
+    // // for (int i = 0; i < 20; i++) {
+    // int[] p = P.removeFirst();
+    // IIG = PROBE(p, ans, IIG);
+    // // if (statusG == -1 || statusG == 1) {
+    // // return G;
     // // }
-    // // }
+    // newp += UPDATEONALLG(IIG);
+    // }
+    // // System.out.println("newp:"+newp);
+    // } while (newp != 0);
+    // return G;
     // }
 
-    static int[][] PROPAGATE(int[][] G, String[][] ans) {
-        int[][] IIG = new int[G.length][];// IIG: 改過的G
-        for (int i = 0; i < G.length; i++)
-            IIG[i] = G[i].clone();
+    // static int UPDATEONALLG(int[][] IIG) {
+    // // 更新所有 G 中像素的狀態
+    // int p = 0;// 更新幾個
+    // for (int i = 0; i < X; i++) {
+    // for (int j = 0; j < X; j++) {
+    // if (G[i][j] == -1 && IIG[i][j] != -1) {
+    // G[i][j] = IIG[i][j];
+    // Gp0[i][j] = IIG[i][j];
+    // Gp1[i][j] = IIG[i][j];
+    // p++;
+    // } else if (G[i][j] == -1 && IIG[i][j] == -1) {
+    // P.add(new int[] { i, j });
+    // }
+    // }
+    // }
+    // return p;
+    // }
+
+    // static int[][] PROBE(int[] p, String[][] ans, int[][] IIG) {
+    // for (int i = 0; i < X; i++) {
+    // for (int j = 0; j < X; j++) {
+    // IIG[i][j] = -1;
+    // }
+    // }
+    // // 1.
+    // Gp0[p[0]][p[1]] = 0; // Gp0猜0
+    // Gp0 = PROPAGATE(ans, IIG, Gp0, statusG0);
+    // // System.out.println("Gp0:\n-------------------");
+    // // for (int i = 0; i < X; i++) {
+    // // for (int j = 0; j < X; j++) {
+    // // System.out.printf("%3d", Gp0[i][j]);
+    // // }
+    // // System.out.println();
+    // // }
+    // // 2.
+    // Gp1[p[0]][p[1]] = 1; // Gp0猜1
+    // Gp1 = PROPAGATE(ans, IIG, Gp1, statusG1);
+    // // System.out.println("Gp1:\n-------------------");
+    // // for (int i = 0; i < X; i++) {
+    // // for (int j = 0; j < X; j++) {
+    // // System.out.printf("%3d", Gp1[i][j]);
+    // // }
+    // // System.out.println();
+    // // }
+    // // 3~10.
+    // if (statusG0 == -1 && statusG1 == -1) {
+    // statusG = -1;
+    // // System.out.println("NOT:" + p[0] + p[1]);
+    // return IIG;
+    // } else if (statusG0 == -1) {
+    // // System.out.println("G1:" + p[0] + p[1]);
+    // for (int i = 0; i < Gp1.length; i++)
+    // IIG[i] = Gp1[i].clone();
+    // } else if (statusG1 == -1) {
+    // // System.out.println("G0:" + p[0] + p[1]);
+    // for (int i = 0; i < Gp0.length; i++)
+    // IIG[i] = Gp0[i].clone();
+    // } else {
+    // // System.out.println("MG:" + p[0] + p[1]);
+    // for (int i = 0; i < X; i++) {
+    // for (int j = 0; j < X; j++) {
+    // IIG[i][j] = MergeC(Gp0[i][j], Gp1[i][j]);
+    // // if (IIG[i][j] != -1)
+    // // System.out.print("{" + i + j + "} ");
+    // }
+    // }
+    // // System.out.println();
+    // }
+    // return IIG;
+    // }
+
+    static int[][] PROPAGATE(int[][] Graph, String[][] ans) {
+        int[][] IIG = new int[Graph.length][];// IIG: 改過的G
+        for (int i = 0; i < Graph.length; i++)
+            IIG[i] = Graph[i].clone();
         // put all rows & clos into LG
         LinkedList<int[]> LG = new LinkedList<>(); // 存G的行跟列
         LinkedList<String> DG = new LinkedList<>(); // 存每行答案
@@ -81,7 +163,7 @@ public class Nonograms {
 
         // 加橫的
         for (int i = 0; i < X; i++) {
-            LG.add(G[i]);
+            LG.add(IIG[i]);
             DG.add(ans[1][i]);
             indexList.add(new int[] { 1, i });
         }
@@ -89,7 +171,7 @@ public class Nonograms {
         for (int j = 0; j < X; j++) {
             int[] a = new int[X];
             for (int i = 0; i < X; i++) {
-                a[i] = G[i][j];
+                a[i] = IIG[i][j];
             }
             LG.add(a);
             DG.add(ans[0][j]);
@@ -100,10 +182,10 @@ public class Nonograms {
         while (!LG.isEmpty()) {
             // System.out.print("stack:");
             // for (int[] elem : indexList) {
-            //     for (int i : elem) {
-            //         System.out.print(i);
-            //     }
-            //     System.out.print(" ");
+            // for (int i : elem) {
+            // System.out.print(i);
+            // }
+            // System.out.print(" ");
             // }
             // System.out.println("");
             // 拿一串格子
@@ -112,12 +194,12 @@ public class Nonograms {
             // 拿一串條件
             String[] d = DG.removeFirst().split(" "); // 拆答案
             LinkedList<Integer> D = new LinkedList<>(); // 裝答案用的list
-            // System.out.print("D:");
+            System.out.print("D:");
             for (String p : d) {// 塞條件進去
                 D.add(Integer.valueOf(p));
-                // System.out.print(p);
+                System.out.print(p);
             }
-            // System.out.println();
+            System.out.println();
 
             // 拿一個位置
             int[] indexInfo = indexList.removeFirst();
@@ -126,14 +208,15 @@ public class Nonograms {
             int DR = D.size(); // j
 
             // 印看看
-            // System.out.print("take: ");
-            // for (int i = 0; i < X; i++) {
-            // System.out.print(L[i] + " ");
-            // }
-            // System.out.println();
+            System.out.print("index " + indexInfo[0] + indexInfo[1] + ": ");
+            for (int i = 0; i < X; i++) {
+                System.out.print(L[i] + " ");
+            }
+            System.out.println();
 
             // 6.
             if (!Fix0(SR, DR, D, L) && !Fix1(SR, DR, D, L)) {
+                System.out.println("Error");
                 statusG = -1;
                 return IIG;
             }
@@ -186,24 +269,24 @@ public class Nonograms {
             }
 
             // 12.檢查填完沒
-            if (allColorsFilled(G)) {
+            if (allColorsFilled(Graph)) {
                 statusG = 1;
             } else {
                 statusG = 0;
             }
             // 試印看看
-            // System.out.println("index:" + indexInfo[0] + indexInfo[1]);
-            // for (int j = 0; j < X; j++) {
-            //     System.out.printf("%3d", newPainted[j]);
-            // }
-            // System.out.println("\n");
-            // for (int i = 0; i < X; i++) {
-            //     for (int j = 0; j < X; j++) {
-            //         System.out.printf("%3d", IIG[i][j]);
-            //     }
-            //     System.out.println();
-            // }
-            // System.out.println("\n");
+            System.out.println("index:" + indexInfo[0] + indexInfo[1]);
+            for (int j = 0; j < X; j++) {
+                System.out.printf("%3d", newPainted[j]);
+            }
+            System.out.println("\n");
+            for (int i = 0; i < X; i++) {
+                for (int j = 0; j < X; j++) {
+                    System.out.printf("%3d", IIG[i][j]);
+                }
+                System.out.println();
+            }
+            System.out.println("\n");
         }
         return IIG;
     }
@@ -213,13 +296,14 @@ public class Nonograms {
     ////////////////////////////////////////////////////////////////////////////////////////
 
     static int[] Paint(int i, int j, LinkedList<Integer> D, int[] L) {
+        // System.out.println("P");
+
         if (i <= 0) {
             return L;
         }
         // Paint'(i,j)
         if (L[i - 1] == 0) {
-            i--;
-            return Paint(i, j, D, L);
+            return Paint0(i, j, D, L);
         } else if (L[i - 1] == 1) {
             return Paint1(i, j, D, L);
         } else {
@@ -237,7 +321,7 @@ public class Nonograms {
             }
             int[] p;
             if (f0 && f1) {
-                p = Merge(p1, p0);
+                p = Merge(p0, p1);
             } else if (f0 && !f1) {
                 p = p0;
             } else if (f1 && !f0) {
@@ -251,47 +335,51 @@ public class Nonograms {
     }
 
     static int[] Paint0(int i, int j, LinkedList<Integer> D, int[] L) {
+        // System.out.println("P0");
         if (i <= 0) {
             return L;
         }
         int[] result = L.clone();
-        result[i - 1] = 0;
+        result[i - 1] = (result[i - 1] == -1) ? 0 : result[i - 1];
         result = Paint(i - 1, j, D, result);
         // System.out.println("P0:" + result[0] + result[1] + result[2] + result[3] +
-        // result[4]);
+        // result[4] + result[5] + result[6] + result[7] + result[8] + result[9]);
         return result;
     }
 
     static int[] Paint1(int i, int j, LinkedList<Integer> D, int[] L) {
-        if (D.isEmpty()) {
+        // System.out.println("P1");
+        if (j <= 0) {
             return L;
         }
-        LinkedList<Integer> d = new LinkedList<>(D);
-        int dj = d.removeLast(); // dj = 要填1的格子數
-        if (i < dj) {
-            return L;
-        }
+        int dj = D.get(--j); // dj = 要填1的格子數
         int[] result = L.clone();
         for (int k = i - 1; k >= i - dj; k--) {
-            result[k] = 1;
+            if (result[k] == -1) {
+                result[k] = 1;
+            } else if (result[k] == 0) {
+                return result;
+            }
         }
-        if (i == dj) {
-            result = Paint(i - dj, j - 1, d, result);
-        } else {
-            result = Paint0(i - dj, j - 1, d, result);
+        if (i != dj) {
+            result = Paint0(i - dj, j, D, result);
         }
         // System.out.println("P1:" + result[0] + result[1] + result[2] + result[3] +
-        // result[4]);
+        // result[4] + result[5] + result[6] + result[7] + result[8] + result[9]);
         return result;
     }
 
     static int[] Merge(int[] s, int[] t) {
         int[] merged = new int[X];
+        // System.out.println("MGp0:" + s[0] + s[1] + s[2] + s[3] +
+        // s[4] + s[5] + s[6] + s[7] + s[8] + s[9]);
+        // System.out.println("MGp1:" + t[0] + t[1] + t[2] + t[3] +
+        // t[4] + t[5] + t[6] + t[7] + t[8] + t[9]);
         for (int k = 0; k < X; k++) {
             merged[k] = MergeC(s[k], t[k]);
         }
         // System.out.println("MG:" + merged[0] + merged[1] + merged[2] + merged[3] +
-        // merged[4]);
+        // merged[4] + merged[5] + merged[6] + merged[7] + merged[8] + merged[9]);
         return merged;
     }
 
@@ -316,6 +404,9 @@ public class Nonograms {
         } else if (i < 0 || j < 0) {
             // System.out.println("Fix0:F2");
             return false; // 防止負值遞歸
+        } else if (L[i - 1] == 1) {
+            // System.out.println("Fix0:F3");
+            return false;
         } else {
             return Fix0(i - 1, j, D, L) || Fix1(i - 1, j, D, L);
         }
@@ -329,12 +420,12 @@ public class Nonograms {
         } else if (i == 0 && j > 0) {
             // System.out.println("Fix1:F1 ");
             return false;
-        } else if (i < 0 || j < 0 || D.isEmpty()) {
+        } else if (i < 0 || j <= 0) {
             // System.out.println("Fix1:F2");
             return false; // 防止負值遞歸或 D 為空
         } else {
-            LinkedList<Integer> d = new LinkedList<>(D);
-            int dj = d.removeLast();
+            int dj = D.get(--j);
+            // System.out.println("d=" + dj);
 
             // 檢查當前條件是否可滿足
             if (i < dj) {
@@ -350,24 +441,12 @@ public class Nonograms {
                 }
             }
 
-            // 確保在 i - dj 之前的格子沒有其他填充條件
-            for (int k = i - dj - 1; k >= 0; k--) {
-                if (L[k] == 1 && d.isEmpty()) {
-                    // System.out.println("Fix1:F5");
-                    return false;
-                } else if (L[k] == 1) {
-                    k -= d.removeLast();
-                    j--;
-                }
+            if (i == dj && j == 0) {
+                // System.out.println("Fix1:T2");
+                return true;
             }
-
-            // 最後一個條件時，確保無需多餘空格
-            if (i == dj || d.isEmpty()) {
-                return Fix1(i - dj, j - 1, d, L) || Fix0(i - dj, j - 1, d, L);
-            }
-
-            // 若不是最後一個條件，嘗試遞歸下一條件
-            return Fix1(i - dj - 1, j - 1, d, L) || Fix0(i - dj - 1, j - 1, d, L);
+            // System.out.println("F1 to F0");
+            return Fix0(i - dj, j, D, L);
         }
     }
 
@@ -375,22 +454,14 @@ public class Nonograms {
     ///
     ///
     /// 下面還沒
-    static int status(Object o) {
-        // 等等再寫我不知道這要幹嘛
-        return 0;
-    }
 
     static void UPDATEONALLG(Object o) {
-        // 這我也不知道是啥先放放
+
     }
 
-    // static void PROBE(int p) {
-    // PROPAGATE(G0);
-    // PROPAGATE(G1);
-    // if (status(G0) == 0 || status(G1) == 0) {
+    static void PROBE(int p) {
 
-    // }
-    // }
+    }
 
     static boolean allColorsFilled(int[][] G) {
         for (int[] row : G) {
